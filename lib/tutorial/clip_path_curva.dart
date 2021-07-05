238ee9e9e55efcd7597d50a9f3ef24e226a7b837
maulana saputra <maulana.saputra@aqi.co.id>
2021-07-05 14:37:12 +0700

    tutorial clip path curva
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

---
+++ lib/tutorial/clip_path_curva.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Custom Clipper"),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: MyClipper(),
+          child: Image(
+            width: 300,
+            image: NetworkImage(
+                "https://pngimage.net/wp-content/uploads/2018/06/pemandangan-png.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0.0, size.height - 50);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 50);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
